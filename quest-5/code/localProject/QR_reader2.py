cd46724e0284ae965e310fd922a9e817cb10228b
Noah <noah.robit@gmail.com>
2023-11-28 18:42:27 +0000

    added changes

--- quest-5/code/localProject/QR_reader2.py
+++ quest-5/code/localProject/QR_reader2.py
@@ -1,19 +1,19 @@
 from PIL import Image
 from pyzbar.pyzbar import decode
 import time
-import numpy as np
 
-loaded_array = np.load('my_array.npy')
+# Load the image
+image_path = 'QR.jpg'  # Replace with the path to your image file
+loaded_image = Image.open(image_path)
 
-data = decode(loaded_array)
-if(data == []):
-    print("not a QR")
+# Decode the QR code
+data = decode(loaded_image)
+
+if not data:
+    print("No QR code found in the image.")
 else:
     print(data[0].data.decode('utf-8'))
-    
-time.sleep(.25)
-#send_udp_data(data[0].data.decode('utf-8'));
 
-
-
+time.sleep(0.25)
+# send_udp_data(data[0].data.decode('utf-8'))
 
--- quest-5/code/localProject/cameraShot2.py
+++ quest-5/code/localProject/cameraShot2.py
@@ -3,23 +3,24 @@
 # Capture a JPEG while still running in the preview mode. When you
 # capture to a file, the return value is the metadata for that image.
 
-import numpy as np
 import time
-import subprocess
-import sys
-import os
+
 from picamera2 import Picamera2, Preview
+from libcamera import controls
 
 picam2 = Picamera2()
+picam2.options["compress_level"] = 6
 
-preview_config = picam2.create_preview_configuration(main={"size": (800, 600)})
+picam2.camera_controls
+picam2.configure(picam2.create_preview_configuration())
+
+preview_config = picam2.create_preview_configuration(main={"size": (3000, 2000)})
 picam2.configure(preview_config)
 
 picam2.start()
 time.sleep(2)
 
-metadata = picam2.capture_array()  
-np.save('my_array.npy', metadata)
+metadata = picam2.capture_file("QR.jpg")
 print(metadata)
 time.sleep(1)
 
